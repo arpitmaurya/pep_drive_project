530e220b43d303f84c052eab6fdaa79d199cd87d
arpit-maurya <arpitmaurya161@gmail.com>
2021-12-30 14:11:33 +0530

    notePad Style update

--- notepad.js
+++ notepad.js
@@ -33,3 +33,49 @@ menu_multiple_notepad.addEventListener('click', () => {
     }
   });
 });
+
+
+let docBold = document.querySelector('.docBold');
+let docItalic = document.querySelector('.docItalic')
+let textArea = document.getElementsByTagName('textarea')
+let docSubSize = document.querySelector('.subSize');
+let addSize = document.querySelector('.addSize');
+let docFontSize = document.querySelector('.textSizeValue');
+let docFontOption = document.querySelector('#fontOption');
+let isBoldDoc = true;
+let isItalicDoc = true;
+
+docBold.addEventListener('click', () => {
+  if (isBoldDoc) {
+    isBoldDoc = false
+        textArea[0].style.fontWeight = 'bold'; 
+  } else {
+    isBoldDoc = true
+        textArea[0].style.fontWeight = ''; 
+  }
+})
+
+docItalic.addEventListener('click', () => {
+  if (isItalicDoc) {
+    isItalicDoc = false;
+    textArea[0].style.fontStyle = 'italic';
+  } else {
+    isItalicDoc = true;
+     textArea[0].style.fontStyle = '';
+  }
+})
+
+
+docSubSize.addEventListener('click', () => {
+  textArea[0].style.fontSize = `${Number(docFontSize.innerHTML) - 1}px`;
+  docFontSize.innerHTML = Number(docFontSize.innerHTML) - 1;
+})
+
+addSize.addEventListener('click', () => {
+   textArea[0].style.fontSize = `${Number(docFontSize.innerHTML) + 1}px`;
+   docFontSize.innerHTML = Number(docFontSize.innerHTML) + 1;
+})
+
+docFontOption.addEventListener('change', (e) => {
+   textArea[0].style.fontFamily = e.target.value
+})
